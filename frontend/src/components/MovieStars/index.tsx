dce85d1e5c33db5640f9f5bc30a2b34959ed402a
Otavio Dias <otaviodias@gmail.com>
2022-01-11 22:57:46 -0400

    movieCard

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,17 @@
+import "./styles.css"
+import {ReactComponent as StarFull} from "assets/img/starFull.svg"
+import {ReactComponent as StarHalf} from "assets/img/starHalf.svg"
+import {ReactComponent as StarEmpty} from "assets/img/starEmpty.svg"
+
+function MovieStars(){
+    return(
+        <div className="dsmovie-stars-container">
+  <StarFull />
+  <StarFull />
+  <StarFull />
+  <StarHalf />
+  <StarEmpty />
+</div>
+    );
+}
+export default MovieStars;
