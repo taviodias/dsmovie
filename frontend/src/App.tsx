898565d2fdda8b092846ca43bf79cff230362822
Otavio Dias <otaviodias@gmail.com>
2022-01-11 22:02:18 -0400

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <h1>Página Principal</h1>
-  );
+  return (<Navbar />);
 }
 
 export default App;
