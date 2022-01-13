d6f7b1d7c46427d81c6c6f9b45419444e2fdb2fa
Otavio Dias <otaviodias@gmail.com>
2022-01-12 21:50:47 -0400

    Find Movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
@@ -0,0 +1,71 @@
+package com.devsuperior.dsmovie.dto;
+
+import com.devsuperior.dsmovie.entities.Movie;
+
+public class MovieDTO {
+	private Long id;
+	private String title, image;
+	private Double score;
+	private Integer count;
+	
+	public MovieDTO() {
+		
+	}
+
+	public MovieDTO(Long id, String title, String image, Double score, Integer count) {
+		this.id = id;
+		this.title = title;
+		this.image = image;
+		this.score = score;
+		this.count = count;
+	}
+	
+	public MovieDTO(Movie movie) {
+		this.id = movie.getId();
+		this.title = movie.getTitle();
+		this.image = movie.getImage();
+		this.score = movie.getScore();
+		this.count = movie.getCount();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+	
+}
