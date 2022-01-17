7685367b83ba4c67ef9afecca8ffb56e41f2cf71
Otavio Dias <otaviodias@gmail.com>
2022-01-16 21:46:57 -0400

    Show Score

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,17 +1,57 @@
 import "./styles.css"
-import {ReactComponent as StarFull} from "assets/img/starFull.svg"
-import {ReactComponent as StarHalf} from "assets/img/starHalf.svg"
-import {ReactComponent as StarEmpty} from "assets/img/starEmpty.svg"
+import { ReactComponent as StarFull } from "assets/img/starFull.svg"
+import { ReactComponent as StarHalf } from "assets/img/starHalf.svg"
+import { ReactComponent as StarEmpty } from "assets/img/starEmpty.svg"
 
-function MovieStars(){
-    return(
+type Props = {
+    score: number
+}
+
+type StarProsps={
+    fill: number
+}
+
+function getFills(score: number) {
+
+    const fills = [0, 0, 0, 0, 0];
+
+    const integerPart = Math.floor(score);
+
+    for (let i = 0; i < integerPart; i++) {
+        fills[i] = 1;
+    }
+
+    const diff = score - integerPart;
+    if (diff > 0.2 && diff < 0.7) {
+        fills[integerPart] = 0.5;
+    }
+    if(diff > 0.7)
+        fills[integerPart] = 1;
+
+    return fills;
+}
+
+function Star({fill}:StarProsps){
+    if(fill === 0)
+        return<StarEmpty/>
+    else if(fill === 0.5)
+        return<StarHalf/>
+    else
+        return<StarFull/>
+}
+
+function MovieStars({ score }: Props) {
+
+    const fills = getFills(score);
+
+    return (
         <div className="dsmovie-stars-container">
-  <StarFull />
-  <StarFull />
-  <StarFull />
-  <StarHalf />
-  <StarEmpty />
-</div>
+            <Star fill={fills[0]} />
+            <Star fill={fills[1]} />
+            <Star fill={fills[2]} />
+            <Star fill={fills[3]} />
+            <Star fill={fills[4]} />
+        </div>
     );
 }
 export default MovieStars;
